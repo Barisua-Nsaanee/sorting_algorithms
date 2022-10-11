798540949c450a1d865027e3f7920ec44eb90cf5
Barisua-Nsaanee <bnsaanee7@gmail.com>
2022-10-11 12:37:03 -0700

    push

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,11 +1,11 @@
-i#include "sort.h"
+#include "sort.h"
 
 /**
  * swap_ints - Swap two integers in an array.
  * @a: The first integer to swap.
  * @b: The second integer to swap.
  */
-vvoid swap_ints(int *a, int *b)
+void swap_ints(int *a, int *b)
 {
 	int tmp;
 
@@ -21,24 +21,24 @@ vvoid swap_ints(int *a, int *b)
  *
  * Description: Prints the array after each swap.
  */
-vvoid bubble_sort(int *array, size_t size)
+void bubble_sort(int *array, size_t size)
 {
 	size_t i, len = size;
-	bool bubbly = false;
+	bool bubly = false;
 
 	if (array == NULL || size < 2)
 		return;
 
-	while (bubbly == false)
+	while (bubly == false)
 	{
-		bubbly = true;
+		bubly = true;
 		for (i = 0; i < len - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
 				swap_ints(array + i, array + i + 1);
 				print_array(array, size);
-				bubbly = false;
+				bubly = false;
 			}
 		}
 		len--;
